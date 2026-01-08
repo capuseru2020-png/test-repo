515d7ee69c4afa71113bcdaa7c8505290988bb67
capuseru2020-png <capuseru2020@gmail.com>
2026-01-08 21:16:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+document.getElementById("calc").onclick = function () {
+  const v = Number(document.getElementById("value").value);
+  document.getElementById("score").textContent = "スコア：" + (v * 2);
+};
+
 document.getElementById("btn").onclick = function () {
   document.getElementById("result").textContent = "JavaScript 動いた";
 };
